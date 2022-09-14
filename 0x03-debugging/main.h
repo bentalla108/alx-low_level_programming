d5a58de061b41f3453dd646c298006c41de9ceb3
Bentalla <tallambaye108@gmail.com>
2022-09-14 22:25:28 +0000

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,24 +1,8 @@
-#ifndef _MAIN_
-#define _MAIN_
-
-int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int);
-int _isalpha(int);
-int _abs(int);
-int print_sign(int n);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+#ifndef MAIN_
+#define MAIN_
 
 void positive_or_negative(int i);
 void putformat(int n)
-
+int largest_number(int a, int b, int c)
+  
 #endif 
