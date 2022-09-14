c0a3f43bc98a9392f7736c250db1e6b3767c924d
Bentalla <tallambaye108@gmail.com>
2022-09-14 22:31:04 +0000

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_
 
 void positive_or_negative(int i);
-void putformat(int n)
-int largest_number(int a, int b, int c)
-  
+void putformat(int n);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
 #endif 
