85cef0fe3090d9a65b8518a81341960f571e2b7a
bentalla108 <tallambaye108@gmail.com>
2022-09-16 00:51:11 +0000

    updating

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef _MAIN_
 #define _MAIN_
-
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,12 +12,10 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-
 void positive_or_negative(int i);
-void putformat(int n)
-
-#endif 
+void putformat(int n);
+void times_table(void);
+#endif
