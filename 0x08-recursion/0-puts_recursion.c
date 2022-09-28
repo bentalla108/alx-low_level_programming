db403e3c6f9cc0ec1d6594200196fc65b41b9afa
bentalla108 <tallambaye108@gmail.com>
2022-09-28 20:41:41 +0000

    Learning recursion on C

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+
+{
+	int i = 0;
+
+	while (*s != '\0')
+		{
+		_putchar(*s),
+		_puts_recursion(char s + 1);
+
+		}
+	_putchar('\n');
+
+	return;
+
+}
