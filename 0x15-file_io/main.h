9d62005a56efbfdf96245928f1e28567e1dc5c5f
Bentalla <tallambaye108@gmail.com>
2022-10-31 20:09:16 +0000

    Adding all library to the main file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,17 @@
  * @Last modified time: 2022-10-31T19:33:39+00:00
  */
 
-ssize_t read_textfile(const char *filename, size_t letters);
+#ifndef FILE_IO
+#define FILE_IO
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
