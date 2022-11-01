22e6cf18e123e12393adc067b5ae47d24db1d5ab
Bentalla <tallambaye108@gmail.com>
2022-11-01 15:25:36 +0000

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,42 +15,42 @@
 * Return: the actual number of letters it could read and print, 0 otherwise
 */
 ssize_t read_textfile(const char *filename, size_t letters)
-  {
-    int let_r, let_w;
-    int fichier;
-    char *text;
-    text = malloc(sizeof(char) * letters);
+{
+int let_r, let_w;
+int fichier;
+char *text;
+text = malloc(sizeof(char) * letters);
 
-    if (filename == NULL)
-    {
-    return (0);
-    }
-    fichier = open(filename, O_RDONLY);
-    if (fichier == -1)
-    {
-    close(fichier);
-    return (0);
-    }
-    if (text == NULL)
-    {
-    close(fichier);
-    return (0);
-    }
+if (filename == NULL)
+{
+return (0);
+}
+fichier = open(filename, O_RDONLY);
+if (fichier == -1)
+{
+close(fichier);
+return (0);
+}
+if (text == NULL)
+{
+close(fichier);
+return (0);
+}
 
-    let_r = read(fichier, text, letters);
-    close(fichier);
+let_r = read(fichier, text, letters);
+close(fichier);
 
-    if (let_r == -1)
-    {
-    free(text);
-    return (0);
-    }
+if (let_r == -1)
+{
+free(text);
+return (0);
+}
 
-    let_w = write(STDOUT_FILENO, text, let_r);
-    free(text);
+let_w = write(STDOUT_FILENO, text, let_r);
+free(text);
 
-    if (let_r != let_w)
-    return (0);
+if (let_r != let_w)
+return (0);
 
-    return (let_w);
-  }
+return (let_w);
+}
