151a502b6be6bb242817158f933cca35aa0a9c42
Bentalla <tallambaye108@gmail.com>
2022-11-01 15:50:30 +0000

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,13 @@
-/**
- * @Author: bentalla
- * @Date:   2022-10-31T19:30:54+00:00
- * @Last modified by:   bentalla
- * @Last modified time: 2022-10-31T19:33:39+00:00
- */
+#ifndef MAIN_H
+#define MAIN_H
 
-#ifndef FILE_IO
-#define FILE_IO
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <stdio.h>
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
